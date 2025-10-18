32642c84e1813ffd7196454742b58d6ea2c80791
lawrencerosin <lawrencerosin@yahoo.com>
2025-10-18 16:29:14 -0400

    User can save recipes in the database

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let matchedUsernames;
 const accounts=database.collection("Accounts");
 const recipes=database.collection("Recipes");
 book.set("view engine", "ejs");
-book.set("views","./templates");
+book.set("views","./templates"); 
 book.get("/createAccount", function(request, response){
     response.render("create account")
 });
@@ -41,13 +41,25 @@ book.use(express.json());
 book.get("/check", async function(request, response){
   
   const signInCheck=await accounts.find({username:request.query.username}).toArray();
-   console.log(request.query.password);
+   
   if(signInCheck.length==0){
-    response.send(`<h1>Failed to Sign In</h1><br/><a href="/signIn">Click here to sign in again.</a>`);
+    response.send(`<script>sessionStorage.setItem("username", ${request.query.username}</script><h1>Failed to Sign In</h1><br/><a href="/signIn">Click here to sign in again.</a>`);
   }
   else{
     response.send(`<h1>Successfully Signed In</h1><br/><a href="/">Click here to create and view a recipe</a>`);
   }
+});
+book.get("/saveAs/:username/:recipeName/:recipe", async function(request, response){
+   await recipes.insertOne({creator:request.params.username, name: request.params.recipeName, ingredients: request.params.recipe});
+  response.send("hello");
+});
+book.post("/saveAs/:username/:recipeName/:recipe", async function(request, response){
+ //await recipes.insertOne({creator:request.params.username, name: request.params.recipeName, ingredients: request.params.recipe});
+});
+book.get("/open/:username/:recipeName", async function(request, response){
+    const opening=recipes.findOne({_id:0, name:1, ingredients:1}, {creator:request.params.username, {name: request.params.recipeName}});
+    response.send("hello");
+  console.log(openings.name);
 });
 book.listen(4000, function(){
   console.log("Running");
